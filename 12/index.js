7557865375e5a25a2765975b0c256fa1ebf633ba
vinicios gabriel <vinigabribarbosa277187@gmail.com>
2022-09-09 19:58:08 -0300

    correçao da questao 3

--- 12/index.js
+++ 12/index.js
@@ -1,9 +1,9 @@
 let m = 90_000
-//ola
+
 let c = 60_000
 
 let n = 24
 
 let taxa = (m / c) ** (1 / n) - 1
 
-console.log(`O seu financiamento de 60000 reais teve uma taxa de juros de ${taxa}, pois após 24 meses você teve que pagar 90000 reais.`)
+console.log(`O seu financiamento de 60000 reais teve uma taxa de juros de ${taxa * 100}, pois após 24 meses você teve que pagar 90000 reais.`)
